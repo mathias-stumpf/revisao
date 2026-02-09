991945415207fe6129e3353aee2b73a68b65cf49
mathiasabreu-spec <mathias.abreu@aluno.senai.br>
2026-02-09 16:45:05 -0300

    first commit

---
+++ Numeros.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace _0902
+{
+    public class Numeros
+    {
+        // int divisao,soma,sub,multi;
+    //     public Numeros(int numero1, int num) 
+    //     {
+    //         this.Numero1 = numero1;
+    // this.Num = num;
+   
+
+                        // public int Numero1 { get; set; }
+        // public int Numero2 { get; set; }
+
+        // public void Somar()
+        // {
+        //     soma = Numero1 + Numero2;
+        //     Console.WriteLine(soma);
+        // }
+        // public void Subtrair()
+        // {
+        //     sub = Numero1 -Numero2;
+        //     Console.WriteLine(sub);
+        // }
+        // public void Divisao()
+        // {
+        //     divisao = Numero1 / Numero2;
+        //     Console.WriteLine(divisao);
+        // }
+        // public void Multiplicacao()
+        // {
+        //     int multi = Numero1 + Numero2;
+        //     Console.WriteLine(multi);
+        // }
+        // public int Num { get; set; }
+
+// public int Dobro()
+// {
+//     int dobro = Num * 2;
+//     return dobro;
+// }
+// public int Triplo()
+// {
+//     int triplo = Num * 3;
+//     return triplo;
+// }
+// public string Nome { get; set; }="";
+// public int Idade { get; set; }
+// public void Chama (){
+//     Console.WriteLine($"bom dia {Nome}");
+// }
+// public void Ida()
+//         {
+//             int idadeis = Idade+10;
+//             Console.WriteLine(Nome+" sua idade d aqui 10 anos sera"+idadeis);
+//         }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+using _0902;
+
+Numeros numeros = new Numeros();
+// Console.WriteLine("digite o primeiro numero");
+// numeros.Numero1=int.Parse(Console.ReadLine());
+// Console.WriteLine("digite o segundo numero");
+// numeros.Numero2=int.Parse(Console.ReadLine());
+// numeros.Somar();
+// numeros.Subtrair();
+// numeros.Multiplicacao();
+// numeros.Divisao();
+// Console.WriteLine("digite seu numero");
+// numeros.Num= int.Parse(Console.ReadLine());
+// Console.WriteLine(numeros.Dobro());
+// // Console.WriteLine(numeros.Triplo());
+// Console.WriteLine("digite seu nome");
+// numeros.Nome= Console.ReadLine();
+// Console.WriteLine("digite sua idade");
+// numeros.Idade= int.Parse(Console.ReadLine());
+// numeros.Chama();
+// numeros.Ida();
+// Console.WriteLine("digite um numero");
+// int numer = int.Parse(Console.ReadLine());
+// if (numer%2 == 0)
+// {
+//     Console.WriteLine("seu numero e par");
+// }
+// else
+// {
+//     Console.WriteLine("seu numero e impar");
+// }
+// int n1 = 6;
+// int n2 = 67;
+// if (n1>n2)
+// {
+//     Console.WriteLine("o primeiro numero e maior");
+// }
+// else
+// {
+//     Console.WriteLine("o segundo numero e maior");
+// }
