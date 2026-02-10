78cf81fe2452b88993de5f566517821f0c95e0f5
Mathis Stumpf Borges de Abreu <452365624@ist.senai>
2026-02-10 16:22:36 -0300

    pinto

--- Program.cs
+++ Program.cs
@@ -41,3 +41,173 @@ Numeros numeros = new Numeros();
 // {
 //     Console.WriteLine("o segundo numero e maior");
 // }
+// Console.WriteLine(" Insira sua nota");
+// double nota = double.Parse(Console.ReadLine());
+
+// if (nota > 7)
+// {
+//     Console.WriteLine("Parabéns, voce passou de ano");
+// }
+
+// else if(nota <7 && nota >4)
+// {
+//     Console.WriteLine("Voce esta de recuperação");
+// }
+
+// else
+// {
+//     Console.WriteLine("Voce estra reprovado");
+// }
+
+// Console.WriteLine("Insira sua idade");
+//  double idade = double.Parse(Console.ReadLine());
+
+//  if (idade < 18)
+//  {
+//     Console.WriteLine("Voce é menor de idade");
+//  }
+
+//  else if( idade > 18 && idade < 60)
+// {
+//     Console.WriteLine("Voce é adulto");
+// }
+// else 
+// {
+//      Console.WriteLine("Voce é idoso");
+// }
+
+// Console.WriteLine("Insira um numero");
+// double numero = double.Parse(Console.ReadLine());
+
+// if (numero == 0)
+// {
+//     Console.WriteLine("Seu numero é igual a zero");
+// }
+// else if (numero>0)
+// {
+//         Console.WriteLine("Seu numero e positivo");
+// }
+// else
+// {
+//         Console.WriteLine("Seu numero e negativo");
+// }
+
+
+// for (int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 1; i <= 50; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+// Console.Write("insira seu número: ");
+// int numero = int.Parse(Console.ReadLine());
+
+// for (int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine($"{numero} x {i} = {numero * i}");
+// }
+
+
+// double numero = 1;
+// double soma = 0;
+
+// while (numero != 0)
+// {
+//     Console.WriteLine("Digite um número (0 para parar): ");
+//     numero = double.Parse(Console.ReadLine());
+
+//     soma += numero;
+// }
+//  Console.WriteLine("Soma total = " + soma);
+// int numerPositi = 0;
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine("digite seu numero" );
+// int numero = int.Parse(Console.ReadLine());
+// if (numero >0)
+//     {
+//         numerPositi += 1;
+//     }
+
+// }
+// Console.WriteLine(numerPositi);
+// Console.WriteLine("digite 1 para somar 2 para subtrair 3 para multiplicar 4 para dividir");
+// int escolha = int.Parse(Console.ReadLine());
+// int resultado = 0;
+// switch (escolha)
+// {
+//     case 1:
+//     Console.WriteLine("digite seus dois numeros");
+//     int n1 = int.Parse(Console.ReadLine());
+//     int n2 = int.Parse(Console.ReadLine());
+//      resultado = n1 + n2;
+//     break;
+//      case 2:
+//     Console.WriteLine("digite seus dois numeros");
+//     n1 = int.Parse(Console.ReadLine());
+//      n2 = int.Parse(Console.ReadLine());
+//      resultado = n1 - n2;
+//     break;
+//     case 3:
+//     Console.WriteLine("digite seus dois numeros");
+//     n1 = int.Parse(Console.ReadLine());
+//      n2 = int.Parse(Console.ReadLine());
+//      resultado = n1 * n2;
+//     break;
+//     case 4:
+//     Console.WriteLine("digite seus dois numeros");
+//     n1 = int.Parse(Console.ReadLine());
+//      n2 = int.Parse(Console.ReadLine());
+//      resultado = n1 / n2;
+//     break;
+// }
+// Console.WriteLine(resultado);
+// Console.WriteLine("digite um numero d 1 a 7");
+// int dia = int.Parse(Console.ReadLine());
+// switch (dia)
+// {
+    
+//     case 1:
+//     Console.WriteLine("domingo"); 
+//     break;
+//      case 2:
+//     Console.WriteLine("segunda"); 
+//     break; 
+//     case 3:
+//     Console.WriteLine("terça"); 
+//     break;
+//      case 4:
+//     Console.WriteLine("quarta"); 
+//     break;
+//      case 5:
+//     Console.WriteLine("quinta"); 
+//     break;
+//      case 6:
+//     Console.WriteLine("sexta"); 
+//     break;
+//      case 7:
+//     Console.WriteLine("sabado"); 
+//     break;
+// }
+Console.WriteLine("digite  1 pra alimento 2 bebida e 3 limpesa");
+int produto = int.Parse(Console.ReadLine());
+switch (produto)
+{
+    case 1:
+    Console.WriteLine("alimento");
+     break;
+       case 2:
+    Console.WriteLine("bebida");
+     break;
+       case 3:
+    Console.WriteLine("limpesa");
+     break;
+    
+}
